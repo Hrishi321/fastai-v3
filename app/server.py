fcd416527b6ba485d7fe6520bc2f4b3d3ac786b5
Hrishi321 <40567890+Hrishi321@users.noreply.github.com>
2020-01-24 03:38:08 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/drive/folders/1zyUT3gPtSfSbY3i17QYAiMJKl6ZOthun?usp=sharing'
+export_file_name = 'resnet50.h5'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['nv', 'mel', 'bkl','bcc','akiec','vasc','df']
 path = Path(__file__).parent
 
 app = Starlette()
